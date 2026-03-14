17f89125e786482c198c5ea454f937773881156c
k.reuzz_ <kauekmirannda@gmail.com>
2026-03-14 14:21:56 -0300

    NOVA FUNÇÃO
    Adicionada a função de calcular a resistência de resistores com 4 ou 5 faixas.

--- escolha.js
+++ escolha.js
@@ -1,4 +1,4 @@
-let funcao = parseInt(prompt("Qual função você deseja fazer?\n1 - Ver o consumo mensal\n2 - Converter"));
+let funcao = parseInt(prompt("Qual função você deseja fazer?\n1 - Ver o consumo mensal\n2 - Converter\n3 - calcular resistores"));
 
 switch (funcao) {
     case 1:
@@ -6,6 +6,9 @@ switch (funcao) {
         break;
     case 2:
         conversao();
+        break;
+    case 3:
+        resistores();
         break;
     default:
         console.log("Opção inválida!");
